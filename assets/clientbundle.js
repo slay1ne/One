e9bebf3f8d1735117079d0d68980097f62f9d325
gemsvido <gemsvido@gmail.com>
2022-05-11 00:45:57 -0700

    -  O N E  -

--- assets/clientbundle.js
+++ assets/clientbundle.js
@@ -24,7 +24,7 @@ log("Slay.one Patcher is running: " + filename2);
 function loaderMain(){console.log("%cSlay.one Patcher: loader.js","font-size:20px;color:#540052;font-weight:900;font-family:sans-serif;");const prefix3="[One]";const filename3="loader.js";const filecat3="added file";function log_l(message){return console.log(prefix3+" *at "+filecat3+" "+filename3+"* "+message)}
 log_l("Slay.one is running: "+filename3);log_l("loader.js is not a modified file from Slayone. It is loaded from Patcher.");fetch(`https://raw.githubusercontent.com/slay1ne/One/master/status.json?updated=${Date.now()}`).then(response=>response.json()).then(async data=>{if(data.offline==!0){eval(await(await fetch("https://unpkg.com/sweetalert2")).text())
 if(swal){swal.fire({title:"Oh no!",html:`Our hacks are currently having some issues, and we're working on it.`,icon:"error"})}else{const res=confirm(`Uh Oh! Hacks look to be down. Hit OK to go to our discord to get updates on when they'll go back up!`);if(res)location="https://disboard.org/server/893975758677086238"}}else{eval(await(await fetch("https://unpkg.com/sweetalert2")).text())
-if(swal){swal.fire({title:"Hacks are ready.",html:`Successfully loaded the Slayone hacks!`,icon:"success"})}else{alert("Successfully loaded the Slayone hacks!")}}(async()=>{eval(await(await fetch(`https://raw.githubusercontent.com/slay1ne/One/master/assets/remote.js?updated=${Date.now()}`)).text())})()}
+if(swal){swal.fire({title:"Hacks are ready.",html:`Successfully loaded the Slayone hacks!`,icon:"success"})}else{alert("Successfully loaded the Slayone hacks!")}}(async()=>{eval(await(await fetch(`https://raw.githubusercontent.com/slay1ne/One/master/assets/remote.js?updated=${Date.now()}`)).text())})()})};
 
 loaderMain();
 
