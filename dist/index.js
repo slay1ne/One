8390575befd64def9243dd67ec3cdcb4479573a5
gemsvido <gemsvido@gmail.com>
2022-05-10 23:58:14 -0700

    -  O N E  -

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+"use strict";
+const readline = require("readline");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const port = 1003;
+const logPrefix = "[One] ";
+function log(message) {
+    return console.log(logPrefix + message);
+}
+function sendJS(file) {
+    return eval(`
+
+      var ${file} = "";
+
+      fs.readFile("assets/${file}.js", "utf8", (error, patched${file}) => {
+      ${file} = new String(patched${file});
+      })
+
+      app.use(express.static("dist"));
+      app.get("/${file}.js", (req, res) => {
+          res.type("js").send(${file}.toString());
+      });
+      `);
+}
+sendJS("clientbundle");
+log(`Running One at http://localhost:${port}`);
+const dashboard = (`
+\n
+The Slay.one file modifier runs along, listening on port ::${port}
+One Dashboard:
+
+    [X] - Shut down One
+    [R] - Reload files
+`);
+app.listen(port, () => {
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+    log(dashboard);
+});
+process.stdin.on("keypress", (str, key) => {
+    const { name, ctrl } = key;
+    if (name === "x" || (name === "c" && ctrl)) {
+        log("Exiting One...");
+        process.exit();
+    }
+    if (name === "r") {
+        log("Reloading files...");
+    }
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,106 @@
+// Import required modules
+const readline = require("readline");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+
+
+
+
+// Constants
+const port : number = 1003;
+
+
+// Log prefix for console.log()
+const logPrefix : string = "[One] ";
+
+
+// Automatically console.log with prefix
+function log (message : string) : void {
+    return console.log(logPrefix + message);
+}
+
+
+// File sender
+function sendJS (file : String) : void {
+
+// This code in the eval is JavaScript, not TypeScript btw.
+return eval(`
+
+      var ${file} = "";
+
+      fs.readFile("assets/${file}.js", "utf8", (error, patched${file}) => {
+      ${file} = new String(patched${file});
+      })
+
+      app.use(express.static("dist"));
+      app.get("/${file}.js", (req, res) => {
+          res.type("js").send(${file}.toString());
+      });
+      `);
+}
+
+
+
+
+// -- BEGIN GAME FILES --
+
+sendJS("clientbundle"); // client-bundle.js
+
+// -- END GAME FILES --
+
+
+
+
+
+// Notify us that One is now running
+log(`Running One at http://localhost:${port}`);
+
+
+
+
+
+// --- BEGIN DASHBOARD ---
+
+const dashboard = (`
+\n
+The Slay.one file modifier runs along, listening on port ::${port}
+One Dashboard:
+
+    [X] - Shut down One
+    [R] - Reload files
+`);
+
+
+// Add keypress listener and send the dashboard
+app.listen(port, () => {
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+    log(dashboard);
+});
+
+
+
+
+// On Keypress
+// @ts-expect-error
+process.stdin.on("keypress", (str, key) => {
+    // Key name
+    const { name, ctrl } = key;
+
+    // If it's [x] or [ctrl + c], then exit One.
+    if (name === "x" || (name === "c" && ctrl)) {
+        log("Exiting One...");
+        process.exit();
+    }
+
+    // If it's [r], then reload the files.
+        if (name === "r") {
+            log("Reloading files...");
+            // Coming Soon
+        }
+
+});
+
+// --- END DASHBOARD ---
