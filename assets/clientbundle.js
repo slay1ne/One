d105e6e1ba989b4cb77c3794d8d0c3d11f4cc184
gemsvido <gemsvido@gmail.com>
2022-05-11 02:19:38 -0700

    -  O N E  -
    Test

--- assets/clientbundle.js
+++ assets/clientbundle.js
@@ -6155,7 +6155,7 @@ loaderMain();
                         else if ("grn" === arr[0]) root_1.root.game.newGrenade(arr), root_1.root.game.replayFile.push(msg);
                         else if ("rl2" === arr[0]) root_1.root.game.reload2(arr), root_1.root.game.replayFile.push(msg);
                         else if ("rsp" === arr[0]) root_1.root.game.playerRespawns(arr), root_1.root.game.replayFile.push(msg);
-                        else if ("ping" === arr[0]) exports.Network.lastPing = Date.now() - exports.Network.lastTimePingSent;
+                        else if ("ping" === arr[0]) { exports.Network.lastPing = Date.now() - exports.Network.lastTimePingSent; root_1.root.game.cureTick = parseInt(arr[1]); }
                         else if ("upg" === arr[0]) root_1.root.game.interface_.presentUpgChoice(arr);
                         else if ("nZ" === arr[0]) root_1.root.game.map === map1_1.map1 ? root_1.root.preStore.newZombies.push({
                             value: arr,
@@ -6233,7 +6233,7 @@ loaderMain();
                             root_1.root.game.setNextMaps(mapVotes)
                         } else if ("map-vote" === arr[0]) root_1.root.game.voteNextMap(parseInt(arr[1]), parseInt(arr[2]));
                         else if ("cure" === arr[0]) root_1.root.game.cureTick = parseInt(arr[1]);
-                        else if ("playerList" === arr[0]) { UI_1.UI.showPlayerList(arr); root_1.root.game.cureTick = parseInt(arr[1]); }
+                        else if ("playerList" === arr[0]) UI_1.UI.showPlayerList(arr);
                         else if ("plInfo" === arr[0]) UI_1.UI.showPlayerInfoWithData(arr);
                         else if ("lvlUp" === arr[0]) { ("regSuc" === arr[0])(0, floatTip_1.floatTip)("success", "Registration complete.", 2e3), (0, viewHelpers_1.hidePopup)("account"); UI_1.UI.lvlUp(arr); }
                         else if ("awardChest" === arr[0]) config_1.playerData.chests = (0, functions_1.parseChests)(arr[1]), resourceBar_1.resourceBar.refresh();
